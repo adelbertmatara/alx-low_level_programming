434cd69229715162b70d3301ea5d14ef48a062e5
adelbertmatara <adelbertmatara@gmail.com>
2022-12-09 15:00:28 -0800

    a program that prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,9 +8,7 @@
 int main(void)
 {
 		int i, j, k;
-		
-		i = 0;
-		
+		i = 0;		
 		while (i < 100)
 		{
 			j = i % 10; /* singles digit */
@@ -20,10 +18,9 @@ int main(void)
 			{
 				putchar(k + '0');															putchar(j + '0');
 
-
 				if (i < 89)																{
-					putchar(44);																putchar(32);																																}																}
-
+					putchar(44);																putchar(32);	
+				}																}
 			i++;
 		}
 		putchar('\n');
