ec77cdac003100a6801ff3a3ccaf16bb65fd6fc6
adelbertmatara <adelbertmatara@gmail.com>
2022-12-09 15:06:40 -0800

    wee

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,8 +8,8 @@
 int main(void)
 {
 		int i, j, k;
-		i = 0;
-				
+
+		i = 0;			
 		while (i < 100)
 		{
 			j = i % 10; /* singles digit */
@@ -18,7 +18,6 @@ int main(void)
 			if (k < j)
 			{
 				putchar(k + '0');															putchar(j + '0');
-
 				if (i < 89)																{
 					putchar(44);																putchar(32);	
 				}																}
